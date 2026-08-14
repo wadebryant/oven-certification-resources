00a09b934fc9ca0495b0d917b12c7baa6d06d3e1
wadebryant <wade.bryant@teammurph.net>
2026-08-14 10:59:08 -0500

    Add basic service worker for installability

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'oven-resources-v1';
+const SHELL = ['/', '/index.html', '/styles.css', '/install.js', '/form.html', '/form.js', '/manifest.webmanifest', '/icon.svg'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).catch(() => {}));
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
